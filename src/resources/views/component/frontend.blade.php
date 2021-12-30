e2cc0677d76d6fed6cc54ad377a355a8d44cc81a
Joe <joe@hkict.com>
2021-12-30 12:23:52 +0800

    update again

--- src/resources/views/component/frontend.blade.php
+++ src/resources/views/component/frontend.blade.php
@@ -21,7 +21,7 @@
             width:100%;
             height:100%;
         }
-        .laravel-livewire2-vide .pointer-events-none{
+        .laravel-livewire2-video .pointer-events-none{
             pointer-events: visible;
         }
     </style>
